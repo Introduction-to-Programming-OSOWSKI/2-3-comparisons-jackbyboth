8986fb15e1ab95acccab90222aa6d5d0550b1a45
jackbyboth23 <817581@mystma.org>
2021-09-28 14:54:40 -0500

    3

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 #WRITE YOUR CODE IN THIS FILE
 def greaterThan(x, y):
-    if x < y:
+    if x > y:
         return True 
     else: 
         return False
 def lessThan(x, y):
-    if x > y:
+    if x < y:
         return True
     else:
         return False
